821dae7b1cb5faa32ebd69e1c9b9c4bf0341e0eb
James Salvatore <jimmy.c.salvatore@gmail.com>
2021-12-17 06:46:56 -0600

    chore: added p3;

--- aoc/p2.py
+++ aoc/p2.py
@@ -1,49 +1,107 @@
 """ 
-Code for https://adventofcode.com/2021/day/1
+Code for https://adventofcode.com/2021/day/2
 """
 
 import os
-from typing import Sequence
+from typing import Sequence, Tuple
+from pydantic.dataclasses import dataclass
 
 import numpy as np
 
-# NOTE: In Numpy 1.22.x, we will be able to use numpy.typing.ArrayLike.
 
+@dataclass
+class Coord:
+    """
+    XY Coordinate with addition and scalar multiplication.
+    (_Note: This is overkill for what we need; I wanted to practice using magic methods._)
 
-def count_increases(data: Sequence[int]) -> int:
-    """Takes `data` (list/ndarray of ints) and calculates the number of times the values increase as the list is waked.
-
-    Examples
-    --------
-    >>> count_increases([1, 2, 3, 2, 4, 1])
-    3
+    Attributes
+    ---------
+    x : int
+    y : int
     """
 
-    return np.sum(np.diff(data) > 0)
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Coord({self.x}, {self.y})"
+
+    def __add__(self, coord):
+        return Coord(self.x + coord.x, self.y + coord.y)
+
+    def __mul__(self, other: int):
+        return Coord(self.x * other, self.y * other)
+
+    def __rmul__(self, other: int):
+        return self.__mul__(other)
 
 
-def create_sliding_window(data: Sequence[int], n: int = 3) -> Sequence[int]:
-    """Creates an `n`-sliding window of a list of ints.
+class Submarine:
+    def __init__(self, directions: list[Tuple[Coord, int]]):
+        self.directions = directions
 
-    Examples
-    --------
-    >>> create_sliding_window([1, 3, 4, 5, 6, 7])
-    [ 8 12 15 18]
+    def commands_part_1(self) -> Coord:
+        """
+        forward X increases the horizontal position by X units.
+        down X increases the depth by X units.
+        up X decreases the depth by X units.
 
-    >>> create_sliding_window([0, 1, 2, 3, 4])
-    [3 6 9]
-    """
-    lag_array = np.array(
-        [[data[idx + jdx] for jdx in range(n)] for idx in range(len(data) - 2)]
-    )
-    return np.sum(lag_array, axis=1)
+        This returns a Coord corresponding to the end location of the submarine.
+        """
+        current_loc = Coord(0, 0)
+        for direction, value in self.directions:
+            current_loc += value * direction
+
+        return current_loc
+
+    def commands_part_2(self) -> Coord:
+        """
+        In addition to horizontal position and depth (see `commands_part_1`), you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+        down X increases your aim by X units.
+        up X decreases your aim by X units.
+        forward X does two things:
+        It increases your horizontal position by X units.
+        It increases your depth by your aim multiplied by X.
+        """
+        current_loc = Coord(0, 0)
+        current_aim = 0
+        for direction, value in self.directions:
+
+            if direction.y in [-1, 1]:  # if going up or down...
+                current_aim += direction.y * value
+
+            elif direction.x == 1:  # if going forward...
+                current_loc += Coord(value, value * current_aim)
+
+            else:
+                raise ValueError(f"Direction {direction} is not a valid direction!")
+
+        return current_loc
+
+    @classmethod
+    def parse_directions(cls, raw_input: str) -> "Submarine":
+        """Takes raw input data from aoc2.csv and parses it into a usable format for the class."""
+
+        DIR_MAP = {"forward": Coord(1, 0), "down": Coord(0, 1), "up": Coord(0, -1)}
+
+        lines = [line.strip().split(" ") for line in raw_input.splitlines()]
+        parsed_dirs = [(DIR_MAP[line[0]], int(line[1])) for line in lines]
+        return cls(directions=parsed_dirs)
 
 
 if __name__ == "__main__":
+    with open(os.path.abspath("aoc/data/a02.csv"), "r") as f:
+        data = f.read().strip()
 
-    data = np.genfromtxt(os.path.abspath("aoc/data/a01.csv"), delimiter=",")
+    submarine = Submarine.parse_directions(data)
 
-    solution_a = count_increases(data)
-    solution_b = count_increases(create_sliding_window(data))
+    result_a = submarine.commands_part_1()
+    solution_a = result_a.x * result_a.y
 
-    print(f"AOC1a: {solution_a}\nAOC1b: {solution_b}")
+    result_b = submarine.commands_part_2()
+    solution_b = result_b.x * result_b.y
+
+    print(f"AOC2a: {solution_a}\nAOC2b: {solution_b}")
---
+++ aoc/p3.py
@@ -0,0 +1,90 @@
+""" 
+Code for https://adventofcode.com/2021/day/3
+"""
+
+import os
+from typing import Sequence, Tuple, Any
+from pydantic.dataclasses import dataclass
+
+import numpy as np
+
+
+class Report:
+    """
+    Class to manage Data for AOC Day 3.
+    """
+
+    def __init__(self, data: np.ndarray):
+        self.data = data
+
+    def calculate_part_1(self) -> int:
+        """Calculates the gamma and epsilon rate for Part 1, returns the product."""
+        gamma_rate = [
+            "1" if Report._mode_is_one(self.data[:, idx]) else "0"
+            for idx in range(len(self.data[0]))
+        ]
+        gamma_rate_val = Report._convert_bin_list_to_int(gamma_rate)
+
+        epsilon_rate = [
+            "0" if Report._mode_is_one(self.data[:, idx]) else "1"
+            for idx in range(len(self.data[0]))
+        ]
+        epsilon_rate_val = Report._convert_bin_list_to_int(epsilon_rate)
+
+        return gamma_rate_val * epsilon_rate_val
+
+    def calculate_part_2(self):
+        """Looks at the mode for each column and picks the corresponding rows which either have mode 1 or 0 (depending on oxy or co2)."""
+        oxy_rating = self.data.copy()
+        co2_rating = self.data.copy()
+
+        # TODO: How to DRY this?
+        idx = 0
+        while len(oxy_rating) > 1 and idx < len(self.data[0]):
+            oxy_desired_bit = int(Report._mode_is_one(oxy_rating[:, idx]))
+            oxy_rating = np.array(
+                [row for row in oxy_rating if row[idx] == oxy_desired_bit]
+            )
+            idx += 1
+
+        idx = 0
+        while len(co2_rating) > 1 and idx < len(self.data[0]):
+            co2_desired_bit = int(not Report._mode_is_one(co2_rating[:, idx]))
+            co2_rating = np.array(
+                [row for row in co2_rating if row[idx] == co2_desired_bit]
+            )
+            idx += 1
+
+        oxy_sol_value = Report._convert_bin_list_to_int(oxy_rating[0])
+        co2_sol_value = Report._convert_bin_list_to_int(co2_rating[0])
+
+        return oxy_sol_value * co2_sol_value
+
+    @staticmethod
+    def _mode_is_one(seq: Sequence) -> bool:
+        """Returns True if the mode of a binary sequence `seq` is 1."""
+        return sum(seq) >= len(seq) / 2
+
+    @staticmethod
+    def _convert_bin_list_to_int(s: list[str]) -> int:
+        """Converts a list of binary values to a decimal.  Eg, '0010' -> 2."""
+        return int("".join(map(str, s)), base=2)
+
+    @classmethod
+    def parse_input(cls, raw_input: str) -> "Report":
+        """Takes raw input data from aoc3.csv (lines of binary) and returns an ndarray where each col/row is the corresponding binary digit from the input."""
+
+        data = np.array([list(map(int, list(row))) for row in raw_input.splitlines()])
+        return cls(data)
+
+
+if __name__ == "__main__":
+    with open(os.path.abspath("aoc/data/a03.csv"), "r") as f:
+        data = f.read()
+
+    report = Report.parse_input(data)
+
+    solution_a = report.calculate_part_1()
+    solution_b = report.calculate_part_2()
+
+    print(f"AOC2a: {solution_a}\nAOC2b: {solution_b}")
